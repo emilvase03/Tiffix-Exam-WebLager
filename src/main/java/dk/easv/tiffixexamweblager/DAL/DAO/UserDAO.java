bb04516704eafbf1e5bb314e10d9376fca93bfc5
Jakob Primdal <jakobprimdal@gmail.com>
2026-04-28 17:01:06 +0200

    Refactor UserDAO now works with new DB schema

--- src/main/java/dk/easv/tiffixexamweblager/DAL/DAO/UserDAO.java
+++ src/main/java/dk/easv/tiffixexamweblager/DAL/DAO/UserDAO.java
@@ -21,10 +21,8 @@ public class UserDAO implements IUserDataAccess {
 
     @Override
     public List<User> getAllUsers() throws Exception {
-
         List<User> users = new ArrayList<>();
-
-        String sql = "SELECT Id, FirstName, LastName, Username, Password, Role FROM [User]";
+        String sql = "SELECT Id, FirstName, LastName, Username, Password, RoleId FROM [User]";
 
         try (Connection conn = databaseConnector.getConnection();
              PreparedStatement stmt = conn.prepareStatement(sql);
@@ -36,15 +34,13 @@ public class UserDAO implements IUserDataAccess {
         }
 
         return users;
-
     }
 
 
     @Override
     public User createUser(User newUser) throws Exception {
-
         String sql = """
-                INSERT INTO [User] (FirstName, LastName, Username, Password, Role)
+                INSERT INTO [User] (FirstName, LastName, Username, Password, RoleId)
                 VALUES (?, ?, ?, ?, ?)
                 """;
 
@@ -55,7 +51,10 @@ public class UserDAO implements IUserDataAccess {
             stmt.setString(2, newUser.getLastName());
             stmt.setString(3, newUser.getUsername());
             stmt.setString(4, newUser.getPassword());
-            stmt.setString(5, newUser.getRole().name()); // ENUM → String
+            if (newUser.getRole().name().equals("ADMIN"))
+                stmt.setInt(5, 1);
+            else
+                stmt.setInt(5, 2);
 
             stmt.executeUpdate();
 
@@ -81,7 +80,7 @@ public class UserDAO implements IUserDataAccess {
 
         String sql = """
                 UPDATE [User]
-                SET FirstName = ?, LastName = ?, Username = ?, Password = ?, Role = ?
+                SET FirstName = ?, LastName = ?, Username = ?, Password = ?, RoleId = ?
                 WHERE Id = ?
                 """;
 
@@ -92,7 +91,10 @@ public class UserDAO implements IUserDataAccess {
             stmt.setString(2, user.getLastName());
             stmt.setString(3, user.getUsername());
             stmt.setString(4, user.getPassword());
-            stmt.setString(5, user.getRole().name()); // ENUM → String
+            if (user.getRole().name().equals("ADMIN"))
+                stmt.setInt(5, 1);
+            else
+                stmt.setInt(5,2);
             stmt.setInt(6, user.getId());
 
             stmt.executeUpdate();
@@ -101,7 +103,6 @@ public class UserDAO implements IUserDataAccess {
 
     @Override
     public void deleteUser(User user) throws Exception {
-
         String sql = "DELETE FROM [User] WHERE Id = ?";
 
         try (Connection conn = databaseConnector.getConnection();
@@ -114,14 +115,12 @@ public class UserDAO implements IUserDataAccess {
 
     @Override
     public boolean usernameExists(String username) throws Exception {
-
         String sql = "SELECT COUNT(*) FROM [User] WHERE LOWER(Username) = LOWER(?)";
 
         try (Connection conn = databaseConnector.getConnection();
              PreparedStatement stmt = conn.prepareStatement(sql)) {
 
             stmt.setString(1, username.trim());
-
             ResultSet rs = stmt.executeQuery();
 
             return rs.next() && rs.getInt(1) > 0;
@@ -129,14 +128,11 @@ public class UserDAO implements IUserDataAccess {
     }
 
     public User getUserByUsername(String username) throws Exception {
-
-        String sql = "SELECT Id, FirstName, LastName, Username, Password, Role FROM [User] WHERE username = ?";
+        String sql = "SELECT Id, FirstName, LastName, Username, Password, RoleId FROM [User] WHERE username = ?";
 
         try (Connection conn = databaseConnector.getConnection();
              PreparedStatement stmt = conn.prepareStatement(sql)) {
-
             stmt.setString(1, username);
-
             ResultSet rs = stmt.executeQuery();
 
             if (rs.next())
@@ -145,23 +141,34 @@ public class UserDAO implements IUserDataAccess {
             return null;
         }
     }
+
     @Override
     public List<User> getUsersByRole(Role role) throws Exception {
-        String sql = "SELECT Id, FirstName, LastName, Username, Password, Role FROM [User] WHERE UPPER(role) = UPPER(?)";
-        List<User> coordinators = new ArrayList<>();
+        String sql = "SELECT Id, FirstName, LastName, Username, Password, RoleId FROM [User] WHERE RoleId = (?)";
+        List<User> users = new ArrayList<>();
         try (Connection conn = databaseConnector.getConnection();
              PreparedStatement stmt = conn.prepareStatement(sql)) {
-            stmt.setString(1, role.name());
+            if (role.name().equals("ADMIN"))
+                stmt.setInt(1, 1);
+            else
+                stmt.setInt(1, 2);
+
             try (ResultSet rs = stmt.executeQuery()) {
                 while (rs.next()) {
-                    coordinators.add(mapUser(rs));
+                    users.add(mapUser(rs));
                 }
             }
         }
-        return coordinators;
+        return users;
     }
 
     private User mapUser(ResultSet rs) throws SQLException {
+        Role role;
+        if (rs.getInt("RoleId") == 1) {
+            role = Role.ADMIN;
+        } else {
+            role = Role.EMPLOYEE;
+        }
 
         return new User(
                 rs.getInt("Id"),
@@ -169,7 +176,7 @@ public class UserDAO implements IUserDataAccess {
                 rs.getString("LastName"),
                 rs.getString("Username"),
                 rs.getString("Password"),
-                Role.valueOf(rs.getString("Role").toUpperCase()) // String → ENUM
+                role
         );
     }
 }
