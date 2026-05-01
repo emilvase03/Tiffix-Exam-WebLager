9dbb9623be07a69870ea847db8c299cac42faf18
Jakob Primdal <jakobprimdal@gmail.com>
2026-05-01 11:47:02 +0200

    Implemented rule selection createProfile component + controller

--- src/main/java/dk/easv/tiffixexamweblager/GUI/Controllers/components/CreateProfileController.java
+++ src/main/java/dk/easv/tiffixexamweblager/GUI/Controllers/components/CreateProfileController.java
@@ -2,24 +2,34 @@ package dk.easv.tiffixexamweblager.GUI.Controllers.components;
 
 // Project imports
 import dk.easv.tiffixexamweblager.BE.Profile;
+import dk.easv.tiffixexamweblager.BE.Rule;
 import dk.easv.tiffixexamweblager.GUI.Controllers.ProfilesTabController;
 import dk.easv.tiffixexamweblager.GUI.Models.ProfileModel;
 import dk.easv.tiffixexamweblager.GUI.Utils.AlertHelper;
 
 // Java imports
+import javafx.collections.ListChangeListener;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
+import org.controlsfx.control.CheckComboBox;
 
-public class CreateProfileController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CreateProfileController implements Initializable {
     @FXML private TextField txtTitle;
     @FXML private Label lblHeader;
+    @FXML private CheckComboBox<Rule> rulesDropdown;
+    @FXML private ListView<Rule> rulesList;
+
     private VBox overlay;
     private ProfileModel profileModel;
     private ProfilesTabController profilesTabController;
     private boolean updateProfile = false;
+    private boolean rulesIsUpdating = false;
     private Profile profileToBeUpdated;
 
     public CreateProfileController() {
@@ -30,10 +40,42 @@ public class CreateProfileController {
         }
     }
 
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        Rule r1 = new Rule(1,"Brightness", 5);
+        rulesDropdown.getItems().add(r1);
+        Rule r2 = new Rule(2, "Rotate 90", 90);
+        rulesDropdown.getItems().add(r2);
+        Rule r3 = new Rule(3, "Rotate 180", 180);
+        rulesDropdown.getItems().add(r3);
+
+        setupOverLay();
+
+    }
+
+    private void setupOverLay() {
+        rulesDropdown.setTitle("Select Rules");
+        rulesDropdown.getCheckModel().getCheckedItems().addListener((ListChangeListener<Rule>) change -> {
+            while (change.next()) {
+                if (change.wasAdded()) {
+                    rulesList.getItems().addAll(change.getAddedSubList());
+                }
+                if (change.wasRemoved()) {
+                    rulesList.getItems().removeAll(change.getRemoved());
+                }
+            }
+        } );
+    }
+
     @FXML
     private void handleSave(ActionEvent event) {
         if (txtTitle.getText().isBlank()) {
             txtTitle.setStyle("-fx-border-color: #FF3D32; -fx-border-width: 1;");
+            return;
+        }
+        if (rulesList.getItems().isEmpty()) {
+            rulesList.setStyle("-fx-border-color: #FF3D32;");
             return;
         }
 
@@ -53,8 +95,7 @@ public class CreateProfileController {
             }
         } else {
             try {
-                Profile profile = new Profile(txtTitle.getText().trim());
-                profileModel.createProfile(profile);
+                Profile profile = profileModel.createProfile(new Profile(txtTitle.getText().trim()));
                 profilesTabController.getTable().getItems().add(profile);
                 txtTitle.clear();
                 handleClose();
