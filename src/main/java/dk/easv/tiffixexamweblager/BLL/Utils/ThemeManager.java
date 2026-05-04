c05632b59687a20617bf2dfb463896056fe95da9
murik <dennyisgodxd@gmail.com>
2026-05-04 11:54:15 +0200

    used java Preferences to save theme locally

--- src/main/java/dk/easv/tiffixexamweblager/BLL/Utils/ThemeManager.java
+++ src/main/java/dk/easv/tiffixexamweblager/BLL/Utils/ThemeManager.java
@@ -5,15 +5,25 @@ import atlantafx.base.theme.NordLight;
 import javafx.application.Application;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
+import java.util.prefs.Preferences;
 
 public class ThemeManager {
 
     private static ThemeManager instance;
+    private static final Preferences prefs = Preferences.userNodeForPackage(ThemeManager.class);
+    private static final String THEME_KEY = "dm";
+
     private final BooleanProperty darkMode = new SimpleBooleanProperty(false);
 
     private ThemeManager() {
-        darkMode.addListener((obs, oldVal, isDark) -> applyTheme(isDark));
-        applyTheme(false); // start with NordLight
+        darkMode.set(prefs.getBoolean(THEME_KEY, false));
+
+        darkMode.addListener((obs, oldVal, isDark) -> {
+            applyTheme(isDark);
+            prefs.putBoolean(THEME_KEY, isDark);
+        });
+
+        applyTheme(darkMode.get());
     }
 
     public static ThemeManager getInstance() {
@@ -29,14 +39,14 @@ public class ThemeManager {
         return darkMode;
     }
 
+    public boolean isDark() {
+        return darkMode.get();
+    }
+
     private void applyTheme(boolean dark) {
         Application.setUserAgentStylesheet(
                 dark ? new NordDark().getUserAgentStylesheet()
                         : new NordLight().getUserAgentStylesheet()
         );
-    }
-
-    public boolean isDark() {
-        return darkMode.get();
     }
 }
