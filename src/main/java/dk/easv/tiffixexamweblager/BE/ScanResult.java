9d89150ec87f8a56343cdd22d958898cb353afad
Jakob Primdal <jakobprimdal@gmail.com>
2026-05-08 09:10:02 +0200

    Refactor: Changed ScanResult class in to a record object

--- src/main/java/dk/easv/tiffixexamweblager/BE/ScanResult.java
+++ src/main/java/dk/easv/tiffixexamweblager/BE/ScanResult.java
@@ -1,22 +1,3 @@
 package dk.easv.tiffixexamweblager.BE;
 
-public class ScanResult {
-
-    private final String fileName;
-    private final byte[] fileBytes;
-
-    public ScanResult(String fileName, byte[] fileBytes) {
-        this.fileName = fileName;
-        this.fileBytes = fileBytes;
-
-    }
-
-    public String getFileName() {
-        return fileName;
-    }
-
-    public byte[] getFileBytes() {
-        return fileBytes;
-    }
-
-}
+public record ScanResult(String fileName, byte[] fileBytes) {}
